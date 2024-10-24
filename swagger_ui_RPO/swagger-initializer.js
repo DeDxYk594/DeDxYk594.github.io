f42dfb71305853db5b8b9bb6585e07d93f9a22e5
DeDxYk594 <zhugeo@mail.ru>
2024-10-25 00:14:37 +0300

    Добавлен swagger ui для просмотра доков для технопарка

---
+++ swagger_ui_RPO/swagger-initializer.js
@@ -0,0 +1,1 @@
+window.onload = function() { window.ui = SwaggerUIBundle({ url: 'https://raw.githubusercontent.com/go-park-mail-ru/2024_2_RPO/refs/heads/swagger_approved/openapi.yaml', dom_id: '#swagger-ui', presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset], layout: 'StandaloneLayout' }); };
